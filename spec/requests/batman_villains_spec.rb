0b7678aaac021c1fbf7d3fdd458877a0fdef64db
Brenden Owens <bowens3@nd.edu>
2019-05-03 14:49:49 -0400

    added index route with tests

--- spec/requests/batman_villains_spec.rb
+++ spec/requests/batman_villains_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe "BatmanVillains", type: :request do
       expect(response). to have_http_status(:created)
     end
   end
+
+  describe "GET /batman_villains" do 
+    it "gets a batman villain" do 
+      headers = {
+        "ACCEPT" => "application/json"
+      }
+
+      get "/batman_villains", :headers => headers
+      expect(response.content_type).to eq ("application/json")
+      expect(response). to have_http_status(:success)
+    end
+  end
 end
