97ec7a0221d81426fc45fcacc734724a4cd7016c
Brenden Owens <bowens3@nd.edu>
2019-05-30 09:09:01 -0400

    added records to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+BatmanVillain.create(:name => "Joker")
+BatmanVillain.create(:name => "Riddler")
+BatmanVillain.create(:name => "Mr. Freeze")
