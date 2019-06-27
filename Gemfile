2b6eb87cf9ce29afbef5744144b23959966af4b3
Brenden Owens <bowens3@nd.edu>
2019-06-27 09:26:12 -0400

    removed parameter store and added secrets manager

--- Gemfile
+++ Gemfile
@@ -56,4 +56,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'aws-sdk-rails'
-gem 'aws-sdk-ssm', '~> 1'
+gem 'aws-sdk-secretsmanager', '~> 1.28'
--- config/application.rb
+++ config/application.rb
@@ -30,19 +30,5 @@ module CodebuildRailsApp
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    if !ENV.has_key?('supersecret')
-      Aws.config.update({
-        region: 'us-east-1',
-        credentials: Aws::InstanceProfileCredentials.new
-      })
-      client = Aws::SSM::Client.new()
-      resp = client.get_parameters({
-        names: ["bowens3-ruby-demo"],
-        with_decryption: true
-      })
-      resp.parameters.each do |param|
-        ENV[param.name.upcase] = param.value
-      end
-    end
   end
 end
