499ef48856d0cc39ddab224a8d2e9aece7b12dd3
Brenden Owens <bowens3@nd.edu>
2019-07-17 15:20:06 -0400

    changed response code

--- app/controllers/secret_controller.rb
+++ app/controllers/secret_controller.rb
@@ -2,6 +2,6 @@ class SecretController < ApplicationController
     def index
         @secrets = {}
         @secrets[:bowens3rubydemo] = ENV["bowens3-ruby-demo".upcase]
-        render json: @secrets, status: 200
+        render json: @secrets, status: 201
     end
 end
