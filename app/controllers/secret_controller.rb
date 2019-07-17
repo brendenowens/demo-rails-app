a67f44a69a2f01022f1cde1ce06786e1355cb18e
Brenden Owens <bowens3@nd.edu>
2019-07-17 15:14:47 -0400

    removed comment

--- app/controllers/secret_controller.rb
+++ app/controllers/secret_controller.rb
@@ -1,6 +1,5 @@
 class SecretController < ApplicationController
     def index
-        #hi
         @secrets = {}
         @secrets[:bowens3rubydemo] = ENV["bowens3-ruby-demo".upcase]
         render json: @secrets, status: 200
