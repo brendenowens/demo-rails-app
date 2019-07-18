e668a953d04ec3a843051e11059c9c3666a65aa9
Brenden Owens <bowens3@nd.edu>
2019-07-18 10:29:07 -0400

    added show route to secrets controller

--- app/controllers/secret_controller.rb
+++ app/controllers/secret_controller.rb
@@ -4,4 +4,9 @@ class SecretController < ApplicationController
         @secrets[:bowens3rubydemo] = ENV["bowens3-ruby-demo".upcase]
         render json: @secrets, status: 200
     end
+
+    def show
+        @secret ={super: "secret"}
+        render json: @secret, status: 200
+    end
 end
